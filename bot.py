10c328c3f4805944132484a6e7036e3c39f0c431
Victor Abdukarimov <viredmi@gmail.com>
2022-12-01 12:02:50 +0300

    Added bot reaction to start command

--- bot.py
+++ bot.py
@@ -10,11 +10,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 quiz_keyboard = [["Новый вопрос", "Сдаться"], ["Мой счет"]]
 reply_markup = ReplyKeyboardMarkup(quiz_keyboard)
 
-def echo(bot, update):
+
+def start_quiz(bot, update):
     chat_id = update.message.from_user.id
     bot.send_message(
         chat_id=chat_id,
-        text=update.message.text,
+        text="Привет! Я бот для викторин!",
         reply_markup=reply_markup
     )
 
@@ -23,7 +24,7 @@ def main():
     load_dotenv()
     updater = Updater(os.getenv("TG_BOT_TOKEN"))
     dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler("start", start_quiz))
     updater.start_polling()
     updater.idle()
 
