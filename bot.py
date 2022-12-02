2712268682975a43d29cfab9380ecee9f5557879
Victor Abdukarimov <viredmi@gmail.com>
2022-12-02 11:39:49 +0300

    Loaded questions from user to redis db

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import random
 import os
 import logging
+from functools import partial
 
 from dotenv import load_dotenv
+import redis
 
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -36,24 +38,38 @@ def start_quiz(bot, update):
     )
 
 
-def send_new_question(bot, update):
-    chat_id = update.message.from_user.id
+def send_new_question(bot, update, redis):
+    user_id = update.message.from_user.id
     quiz = get_questions_and_answers_from_file("120br2.txt")
     if update.message.text == "Новый вопрос":
         question = random.choice(list(quiz.keys()))
         bot.send_message(
-            chat_id=chat_id,
+            chat_id=user_id,
             text=question,
             reply_markup=reply_markup
         )
+        redis_set = redis.set(user_id, question)
+        logger.info(redis_set)
+        redis_get = redis.get(user_id)
+        logger.info(redis_get)
 
 
 def main():
     load_dotenv()
+    redis_db = redis.Redis(
+        host=os.getenv("REDIS_DB"),
+        port=os.getenv("REDIS_PORT"),
+        password=os.getenv("REDIS_PASSWORD"),
+        decode_responses=True
+    )
     updater = Updater(os.getenv("TG_BOT_TOKEN"))
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start_quiz))
-    dp.add_handler(MessageHandler(Filters.text, send_new_question))
+    dp.add_handler(
+        MessageHandler(
+            Filters.text, partial(send_new_question, redis=redis_db)
+        )
+    )
     updater.start_polling()
     updater.idle()
 
