f9e6df58eb8b275a8da4533ac39e091e7c7ec453
Victor Abdukarimov <viredmi@gmail.com>
2022-12-05 20:21:51 +0300

    Deleted unusable func

--- get_quiz.py
+++ get_quiz.py
@@ -1,11 +1,6 @@
 import os
 
 
-for root, dirs, files in os.walk("./quiz-questions"):
-    for file in files:
-        pass
-
-
 def get_questions_and_answers_from_file(file_name):
     quiz = {}
     with open(
@@ -18,7 +13,4 @@ def get_questions_and_answers_from_file(file_name):
                 question = text_peace 
             if text_peace.startswith("Ответ"):
                 quiz[question] = text_peace 
-
-
-if __name__ == "__main__":
-    get_questions_and_answers_from_file("120br2.txt")
+    return quiz
